c5caf142adc48b289b3489d86a5839447e9ba246
Beatriz <beatriz.lagoperez102@gmail.com>
2022-03-27 23:49:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/reto3/MapActivity.java
@@ -0,0 +1,94 @@
+package com.example.reto3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+public class MapActivity extends AppCompatActivity implements LocationListener {
+    protected LocationManager locationManager;
+    protected Context context;
+    private MapView map;
+    private IMapController mapController;
+    private Button btn;
+    private Double latitud, longitud;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET}, 1);
+        }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 0, this);
+
+        //This prepares OpenStreetMaps
+        context = getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
+
+        setContentView(R.layout.activity_map);
+
+        //This renders the Map
+        map = findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+        mapController = map.getController();
+        mapController.setZoom(20.00);
+        GeoPoint startPoint = new GeoPoint(20.6133105, -100.4052627); //Hardcoded to Queretaro
+        mapController.setCenter(startPoint);
+
+        btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager smsManager = SmsManager.getDefault();
+                if(latitud != null && longitud != null) {
+                    System.out.println("HOLA VOY A MANDAR EL SMS");
+                    String msg = "Estoy en las coordenadas " + latitud.toString() + ", " + longitud.toString();
+                    smsManager.sendTextMessage("4427217514", null, msg, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(context, "The location is not avaiable, can't send the SMS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitud = location.getLatitude();
+        longitud = location.getLongitude();
+        GeoPoint point = new GeoPoint(latitud,longitud);
+        mapController.setCenter(point);
+        Marker marker = new Marker(map);
+        marker.setPosition(point);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(marker);
+    }
+}
